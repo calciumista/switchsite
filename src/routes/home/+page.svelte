356a2b18a15ed0d5cc10b0739e3bffd6e3933eee
calcalayy <minelandsmc14@gmail.com>
2023-07-21 18:19:06 +0300

    poop

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -21,7 +21,7 @@
         </a>
       </div>
       <div class="align-bottom">
-        <p class="text-white text-sm text-center">made by @calciumista on disc github.com/calciumista/switchsite</p>
+        <p class="text-white text-sm text-center">github.com/calciumista/switchsite</p>
     </div>
 </div>
 
