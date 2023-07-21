35d7dc42f30c171131d454acb06d12a28df9ae6f
calcalayy <minelandsmc14@gmail.com>
2023-07-21 18:17:11 +0300

    rawr uwu xd u so warm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,4 @@
     <h1 class="text-white font-bold text-lg text-center">switch homebrew central</h1>
     <p class="text-white text-center py-1">the best place for info on switch homebrew</p>
     <a href="/home" class="text-white font-bold text-center text-lg hover:scale-125 transition-all">enter</a>
-    <div class="align-bottom">
-        <p class="text-white text-sm text-center">made by @calciumista on disc github.com/calciumista/switchsite</p>
-      </div>
 </div>
