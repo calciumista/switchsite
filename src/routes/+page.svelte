f5111fa5aefab79682f4869b791a8acc43c034db
calcalayy <minelandsmc14@gmail.com>
2023-07-22 09:49:24 +0300

    fixed various tings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="flex flex-col min-h-screen justify-center bg-red-600">
     <h1 class="text-white font-bold text-lg text-center">switch homebrew central</h1>
     <p class="text-white text-center py-1">the best place for info on switch homebrew</p>
-    <a href="/home" class="text-white font-bold text-center text-lg hover:scale-125 transition-all">enter</a>
+    <a href="/home" class="text-white font-bold text-lg hover:scale-125 transition-all duration-300 text-center ml-auto mr-auto">enter</a>
 </div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,20 +3,20 @@
     <div class="container m-auto grid grid-cols-3 gap-5">
         <a href="/home/post/90dns">
             <div class="tile bg-white rounded hover:scale-105 transition-all">
-                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all">90dns - why you shouldnt use it.</h1>
-                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all">by calcium</p>
+                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all duration-300">90dns - why you shouldnt use it.</h1>
+                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all duration-300">by calcium</p>
               </div>
         </a>
         <a href="/home/post/ban">
             <div class="tile bg-white rounded hover:scale-105 transition-all">
-                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all">best ban protection for switch</h1>
-                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all">by calcium</p>
+                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all duration-300">best ban protection for switch</h1>
+                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all duration-300">by calcium</p>
               </div>
         </a>
         <a href="/home/post/links">
             <div class="tile bg-white rounded hover:scale-105 transition-all">
-                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all">good homebrew links</h1>
-                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all">by calcium</p>
+                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all duration-300">good homebrew links</h1>
+                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all duration-300">by calcium</p>
               </div>
         </a>
       </div>
