8c0656ebae4c867da1d2430f6de184be9cf4f820
calcalayy <minelandsmc14@gmail.com>
2023-07-22 14:32:26 +0300

    twerking

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,20 +3,20 @@
     <div class="container m-auto grid grid-cols-3 gap-5">
         <a href="/home/post/90dns">
             <div class="tile bg-white rounded hover:scale-105 transition-all hover:bg-blue-500">
-                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all duration-300">90dns - why you shouldnt use it.</h1>
-                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all duration-300">by calcium</p>
+                <h1 class="text-left text-black font-bold pl-2 pt-2 duration-300 hover:text-white">90dns - why you shouldnt use it.</h1>
+                <p class="text-left text-neutral-500 pl-2 pb-2 hoduration-300 hover:text-white">by calcium</p>
               </div>
         </a>
         <a href="/home/post/ban">
             <div class="tile bg-white rounded hover:scale-105 transition-all hover:bg-blue-500">
-                <h1 class="text-left text-black font-bold pl-2 pt-2 ">best ban protection for switch</h1>
-                <p class="text-left text-neutral-500 pl-2 pb-2 ">by calcium</p>
+                <h1 class="text-left text-black font-bold pl-2 pt-2 duration-300 hover:text-white">best ban protection for switch</h1>
+                <p class="text-left text-neutral-500 pl-2 pb-2 duration-300 hover:text-white">by calcium</p>
               </div>
         </a>
         <a href="/home/post/links">
             <div class="tile bg-white rounded hover:scale-105 transition-all hover:bg-blue-500">
-                <h1 class="text-left text-black font-bold pl-2 pt-2 hover:scale-[1.015] transition-all duration-300">good homebrew links</h1>
-                <p class="text-left text-neutral-500 pl-2 pb-2 hover:scale-[1.015] transition-all duration-300">by calcium</p>
+                <h1 class="text-left text-black font-bold pl-2 pt-2 transition-all duration-300 hover:text-white">good homebrew links</h1>
+                <p class="text-left text-neutral-500 pl-2 pb-2 transition-all duration-300 hover:text-white">by calcium</p>
               </div>
         </a>
       </div>
