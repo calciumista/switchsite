b5012d6660a94778dd91cc7e3046df3c086de3a1
calcalayy <minelandsmc14@gmail.com>
2023-07-25 11:16:02 +0300

    added rmandom sTOOF <3

--- src/routes/home/post/[slug]/+page.svelte
+++ src/routes/home/post/[slug]/+page.svelte
@@ -6,7 +6,6 @@
 <div class="flex flex-col min-h-screen justify-center bg-red-600">
     <h1 class="text-white font-bold text-lg text-center py-2">{data.title}</h1>
     <p class="text-white text-sm text-center">{@html data.content}</p>
-    <div class="align-bottom">
-        <a class="text-white text-sm text-center flex justify-center" href="https://github.com/calciumista/switchsite">github.com/calciumista/switchsite</a>
-    </div>
+
+    <a href="/home" class="text-white font-bold text-lg hover:scale-125 transition-all duration-300 text-center ml-auto mr-auto py-4">back</a>
 </div>
