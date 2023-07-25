b72d322aa2e1a61e7114c673214753347f4f8baa
calcalayy <minelandsmc14@gmail.com>
2023-07-25 10:48:00 +0300

    added rmandom sTOOF <3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,4 @@
     <p class="text-white text-center py-1">the best place for info on switch homebrew</p>
     <a href="/home" class="text-white font-bold text-lg hover:scale-125 transition-all duration-300 text-center ml-auto mr-auto">enter</a>
 </div>
+
--- src/routes/home/post/[slug]/+page.svelte
+++ src/routes/home/post/[slug]/+page.svelte
@@ -6,4 +6,7 @@
 <div class="flex flex-col min-h-screen justify-center bg-red-600">
     <h1 class="text-white font-bold text-lg text-center py-2">{data.title}</h1>
     <p class="text-white text-sm text-center">{@html data.content}</p>
+    <div class="align-bottom">
+        <a class="text-white text-sm text-center flex justify-center" href="https://github.com/calciumista/switchsite">github.com/calciumista/switchsite</a>
+    </div>
 </div>
